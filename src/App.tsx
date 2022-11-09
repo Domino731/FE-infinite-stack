532e4f45247ed3c02681f053b13dca4a721a560b
Domino731 <d.orzechowski18@gmail.com>
2022-11-09 18:19:08 +0100

    redux init

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,20 @@ import {
 } from "react-router-dom";
 import {APP_ROUTES} from "./Routes/const";
 import {Login, Register} from "./Containers/Auth";
+import {Provider} from 'react-redux'
+import {store} from "./Redux";
 
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path={APP_ROUTES.HOME} element={<Login/>}/>
-                    <Route path={APP_ROUTES.REGISTER} element={<Register/>}/>
-                </Routes>
-            </BrowserRouter>
-        </ThemeProvider>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path={APP_ROUTES.HOME} element={<Login/>}/>
+                        <Route path={APP_ROUTES.REGISTER} element={<Register/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider></Provider>
     );
 }
 
