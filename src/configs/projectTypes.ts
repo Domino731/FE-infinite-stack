78a7d00a455f1c097657955421f357ae9a195455
Domino731 <d.orzechowski18@gmail.com>
2022-12-06 18:21:22 +0100

    project types config

---
+++ src/configs/projectTypes.ts
@@ -0,0 +1,48 @@
+export const PROJECT_TYPES = [
+    {
+        type: 'software',
+        label: 'Software',
+        categories: [
+            {
+                category: 'WEBSITE',
+                label: 'Website'
+            },
+            {
+                category: 'WEB_APP',
+                label: 'Web app'
+            },
+            {
+                category: 'WEB_GAME',
+                label: 'Web game'
+            },
+            {
+                category: 'WEB_SHOP',
+                label: 'Web shop'
+            },
+            {
+                category: 'MOBILE_APP',
+                label: 'Mobile app'
+            },
+            {
+                category: 'MOBILE_GAME',
+                label: 'Mobile game'
+            },
+            {
+                category: 'WEB_APP',
+                label: 'Web app'
+            },
+            {
+                category: 'SYSTEM_SOFTWARE',
+                label: 'System software'
+            },
+            {
+                category: 'UTILITY_SOFTWARE',
+                label: 'Utility software'
+            },
+            {
+                category: 'APPLICATION_SOFTWARE',
+                label: 'Application software'
+            },
+        ]
+    }
+]
