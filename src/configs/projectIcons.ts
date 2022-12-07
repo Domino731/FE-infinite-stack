269f59247c32970a89269d6cedcdffbb42f7d215
Domino731 <d.orzechowski18@gmail.com>
2022-12-07 17:19:13 +0100

    icons for project

---
+++ src/configs/projectIcons.ts
@@ -0,0 +1,75 @@
+import {FaRocket} from "react-icons/fa"
+import {IoIosApps} from 'react-icons/io'
+import {CgWebsite} from 'react-icons/cg'
+import {BsFillPhoneFill} from 'react-icons/bs'
+import {GrInsecure} from 'react-icons/gr'
+import {AiFillCar, AiFillCloud} from 'react-icons/ai'
+import {RiComputerFill} from 'react-icons/ri'
+import {SiPaloaltosoftware} from 'react-icons/si';
+import {GrRun} from 'react-icons/gr'
+import {FaCat, FaKeyboard} from 'react-icons/fa'
+import {GrTest, GrDiamond} from 'react-icons/gr';
+import {GiUfo} from 'react-icons/gi'
+
+export const ProjectIcons = [
+    {
+        key: 'rocket',
+        icon: FaRocket
+    },
+    {
+        key: 'io',
+        icon: IoIosApps
+    },
+    {
+        key: 'website',
+        icon: CgWebsite
+    },
+    {
+        key: 'phone',
+        icon: BsFillPhoneFill
+    },
+    {
+        key: 'secure',
+        icon: GrInsecure
+    },
+    {
+        key: 'car',
+        icon: AiFillCar
+    },
+    {
+        key: 'computer',
+        icon: RiComputerFill
+    },
+    {
+        key: 'cloud',
+        icon: AiFillCloud
+    },
+    {
+        key: 'software',
+        icon: SiPaloaltosoftware
+    },
+    {
+        key: 'run',
+        icon: GrRun
+    },
+    {
+        key: 'cat',
+        icon: FaCat
+    },
+    {
+        key: 'test',
+        icon: GrTest
+    },
+    {
+        key: 'keyboard',
+        icon: FaKeyboard
+    },
+    {
+        key: 'diamond',
+        icon: GrDiamond
+    },
+    {
+        key: 'ufo',
+        icon: GiUfo
+    }
+]
