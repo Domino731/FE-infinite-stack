637e38de42814871c1fea4b012caee1703836db8
Domino731 <d.orzechowski18@gmail.com>
2022-12-10 14:40:56 +0100

    switching between sections in create new project page

--- src/Containers/CreateNewProject/index.tsx
+++ src/Containers/CreateNewProject/index.tsx
@@ -4,15 +4,15 @@ import Divider from "../../Components/Divider";
 import {Button} from "../../Components/Button";
 import Flex from "../../Components/Flex";
 import {Status} from "./components/Status";
-import {Formik} from "formik";
-import {FormEvent, useMemo, useState} from "react";
+import {Formik, FormikValues} from "formik";
+import {FormEvent, useCallback, useMemo, useState} from "react";
 import {CREATE_NEW_PROJECT_SECTIONS_DATA} from "./const";
 import {DetailsInitialValues, DetailsValidationSchema} from "./formik";
 import {CREATE_NEW_PROJECT_SECTIONS, CreateNewProjectSectionsUnion} from "./types";
 
 const CreateNewProject = () => {
     // default is details
-    const [step, setStep] = useState(CREATE_NEW_PROJECT_SECTIONS_DATA[0].key)
+    const [step, setStep] = useState<CreateNewProjectSectionsUnion>(CREATE_NEW_PROJECT_SECTIONS_DATA[0].key)
     // holding all data about project - details, team
     const [data, setData] = useState<Record<CreateNewProjectSectionsUnion, Record<string, any> | null>>({
             [CREATE_NEW_PROJECT_SECTIONS.DETAILS]: null,
@@ -25,9 +25,15 @@ const CreateNewProject = () => {
 
     // data about what steps are completed for new project
     const completedSteps = useMemo((): Array<CreateNewProjectSectionsUnion> => {
-        return [CREATE_NEW_PROJECT_SECTIONS.DETAILS]
-        // return Object.entries(data).filter(([_, value]) => Boolean(value)).map(([key]) => key as CreateNewProjectSectionsUnion);
+        return Object.entries(data).filter(([_, value]) => Boolean(value)).map(([key]) => key as CreateNewProjectSectionsUnion);
     }, [data])
+
+    const handleSubmit = useCallback((formikValues: FormikValues) => {
+        const stepIndex: number = CREATE_NEW_PROJECT_SECTIONS_DATA.findIndex(({key}) => key === step);
+        const nextStep = CREATE_NEW_PROJECT_SECTIONS_DATA[stepIndex + 1].key;
+        setData(prevState => ({...prevState, [step]: formikValues}));
+        setStep(nextStep)
+    }, [step])
 
     return <Box display="flex" justifyContent="center" alignItems="center" w="100%" h="100vh" bgGradient="blue">
         <Box p="26" bR="xl" w="700px" h="800px" bgColor="white" position="relative">
@@ -37,7 +43,7 @@ const CreateNewProject = () => {
             <Formik
                 initialValues={DetailsInitialValues}
                 validationSchema={DetailsValidationSchema}
-                onSubmit={(e) => console.log(1)}
+                onSubmit={handleSubmit}
             >
                 {(formikProps) => <form onSubmit={(event: FormEvent<HTMLFormElement>) => {
                     event.preventDefault();
