418325338c30af4c3382b49e06c3bc9a093add6a
Domino731 <d.orzechowski18@gmail.com>
2022-12-11 20:33:28 +0100

    roles by category config

--- src/Containers/CreateNewProject/sections/People.tsx
+++ src/Containers/CreateNewProject/sections/People.tsx
@@ -1,5 +1,6 @@
 export const CreateNewProjectPeople = () => {
     return <>
-
+        Good and completed team is a half of success, choose your developers, management and testing people.
+        
     </>
 }
---
+++ src/configs/roles.ts
@@ -0,0 +1,27 @@
+// data from https://www.aha.io/roadmapping/guide/information-technology/it-job-titles
+export const roles = [
+    {
+        area: 'architecture',
+        label: 'Architecture'
+    },
+    {
+        area: 'security',
+        label: 'Security'
+    },
+    {
+        area: 'network_and_system_administration',
+        label: 'Network and system administration'
+    },
+    {
+        area: 'developers',
+        label: 'Developers'
+    },
+    {
+        area: 'database',
+        label: 'Database'
+    },
+    {
+        area: 'business_analysis',
+        label: 'Business analysis'
+    }
+]
