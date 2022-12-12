0f0220554c43c4e69d985b4c2ab0408638e9e1a9
Domino731 <d.orzechowski18@gmail.com>
2022-12-12 21:34:40 +0100

    jobs for each role

--- src/configs/roles.ts
+++ src/configs/roles.ts
@@ -1,27 +1,317 @@
 // data from https://www.aha.io/roadmapping/guide/information-technology/it-job-titles
+// TODO c: maybe description for each job?
 export const roles = [
     {
-        area: 'architecture',
-        label: 'Architecture'
+        key: 'architecture',
+        label: 'Architecture',
+        jobs: [
+            {
+                key: 'cloud_infrastructure_architect',
+                label: 'Cloud infrastructure architect'
+            },
+            {
+                key: 'enterprise_architect',
+                label: 'Enterprise architect'
+            },
+            {
+                key: 'it_systems_architect',
+                label: 'IT systems architect'
+            },
+            {
+                key: 'solutions_architect',
+                label: 'Solutions architect'
+            },
+            {
+                key: 'technical_architect',
+                label: 'Technical architect'
+            }
+        ]
     },
     {
-        area: 'security',
-        label: 'Security'
+        key: 'security',
+        label: 'Security',
+        jobs: [
+            {
+                key: 'Application security administrator',
+                label: 'Application security administrator'
+            },
+            {
+                key: 'Artificial intelligence security specialist',
+                label: 'Artificial intelligence security specialist'
+            },
+            {
+                key: 'Cloud security specialist',
+                label: 'Cloud security specialist'
+            },
+            {
+                key: 'Cybersecurity hardware engineer',
+                label: 'Cybersecurity hardware engineer'
+            },
+            {
+                key: 'Cyber intelligence specialist',
+                label: 'Cyber intelligence specialist'
+            },
+            {
+                key: 'Cryptographer',
+                label: 'Cryptographer'
+            },
+            {
+                key: 'Data privacy officer',
+                label: 'Data privacy officer'
+            },
+            {
+                key: 'Digital forensics analyst',
+                label: 'Digital forensics analyst'
+            },
+            {
+                key: 'IT security engineer',
+                label: 'IT security engineer'
+            },
+            {
+                key: 'Information assurance analyst',
+                label: 'Information assurance analyst'
+            },
+            {
+                key: 'Security systems administrator',
+                label: 'Security systems administrator'
+            }
+        ]
     },
     {
-        area: 'network_and_system_administration',
-        label: 'Network and system administration'
+        key: 'network_and_system_administration',
+        label: 'Network and system administration',
+        jobs: [
+            {
+                key: 'computer_systems_manager',
+                label: 'Computer systems manager'
+            },
+            {
+                key: 'network_architect',
+                label: 'Network architect'
+            },
+            {
+                key: 'systems_analyst',
+                label: 'Systems analyst'
+            },
+            {
+                key: 'it_coordinator',
+                label: 'IT coordinator'
+            }, {
+                key: 'network_administrator',
+                label: 'Network administrator'
+            },
+            {
+                key: 'network_engineer',
+                label: 'Network engineer'
+            },
+            {
+                key: 'system_administrator',
+                label: 'System administrator'
+            },
+            {
+                key: 'wireless_network_engineer',
+                label: 'Wireless network engineer'
+            },
+        ]
     },
     {
-        area: 'developers',
-        label: 'Developers'
+        key: 'developers',
+        label: 'Developers',
+        jobs: [
+            {
+                key: 'Back-end developer',
+                label: 'Back-end developer'
+            },
+            {
+                key: 'Cloud/software architect',
+                label: 'Cloud/software architect'
+            }, {
+                key: 'Cloud/software developer',
+                label: 'Cloud/software developer'
+            }, {
+                key: 'wireless_network_engineer',
+                label: 'Wireless network engineer'
+            },
+            {
+                key: 'Cloud/software applications engineer',
+                label: 'Cloud/software applications engineer'
+            },
+            {
+                key: 'Cloud system administrator',
+                label: 'Cloud system administrator'
+            },
+            {
+                key: 'Cloud system engineer',
+                label: 'Cloud system engineer'
+            },
+            {
+                key: 'DevOps engineer',
+                label: 'DevOps engineer'
+            },
+            {
+                key: 'Front-end developer',
+                label: 'Front-end developer'
+            },
+            {
+                key: 'Full-stack developer',
+                label: 'Full-stack developer'
+            },
+            {
+                key: 'Java developer',
+                label: 'Java developer'
+            },
+            {
+                key: 'Platform engineer',
+                label: 'Platform engineer'
+            },
+            {
+                key: 'Release manager',
+                label: 'Release manager'
+            },
+            {
+                key: 'Reliability engineer',
+                label: 'Reliability engineer'
+            },
+            {
+                key: 'Software engineer',
+                label: 'Software engineer'
+            },
+            {
+                key: 'Software quality assurance analyst',
+                label: 'Software quality assurance analyst'
+            },
+            {
+                key: 'UI (user interface) designer',
+                label: 'UI (user interface) designer'
+            },
+            {
+                key: 'Software quality assurance analyst',
+                label: 'Software quality assurance analyst'
+            },
+            {
+                key: 'UX (user experience) designer',
+                label: 'UX (user experience) designer'
+            },
+            {
+                key: 'Web developer',
+                label: 'Web developer'
+            },
+        ]
     },
     {
-        area: 'database',
-        label: 'Database'
+        key: 'database',
+        label: 'Database',
+        jobs: [
+            {
+                key: 'Database administrator',
+                label: 'Database administrator'
+            },
+            {
+                key: 'Database analyst',
+                label: 'Database analyst'
+            },
+            {
+                key: 'Data quality manager',
+                label: 'Data quality manager'
+            },
+            {
+                key: 'Database report writer',
+                label: 'Database report writer'
+            },
+            {
+                key: 'SQL database administrator',
+                label: 'SQL database administrator'
+            },
+        ]
     },
     {
-        area: 'business_analysis',
-        label: 'Business analysis'
+        key: 'business_analysis',
+        label: 'Business analysis',
+        jobs: [
+            {
+                key: 'Big data engineer/architect',
+                label: 'Big data engineer/architect'
+            },
+            {
+                key: 'Business intelligence specialist/analyst',
+                label: 'Business intelligence specialist/analyst'
+            },
+            {
+                key: 'Business systems analyst',
+                label: 'Business systems analyst'
+            },
+            {
+                key: 'Data analyst',
+                label: 'Data analyst'
+            },
+            {
+                key: 'Data analytics developer',
+                label: 'Data analytics developer'
+            },
+            {
+                key: 'Data modeling analyst',
+                label: 'Data modeling analyst'
+            },
+            {
+                key: 'Data scientist',
+                label: 'Data scientist'
+            },
+            {
+                key: 'Data warehouse manager',
+                label: 'Data warehouse manager'
+            },
+            {
+                key: 'Data warehouse programming specialist',
+                label: 'Data warehouse programming specialist'
+            },
+            {
+                key: 'Intelligence specialist',
+                label: 'Intelligence specialist'
+            }
+        ]
+    },
+    {
+        key: 'support',
+        label: 'Help desk and customer support job titles',
+        jobs: [
+            {
+                key: 'Help desk support specialist',
+                label: 'Help desk support specialist'
+            },
+            {
+                key: 'IT support specialist',
+                label: 'IT support specialist'
+            },
+            {
+                key: 'Customer service representative',
+                label: 'Customer service representative'
+            }
+        ]
+    },
+    {
+        key: 'management',
+        label: 'Project and product management job titles',
+        jobs: [
+            {
+                key: 'Technical product manager',
+                label: 'Technical product manager'
+            },
+            {
+                key: 'Product manager',
+                label: 'Product manager'
+            },
+            {
+                key: 'Project manager',
+                label: 'Project manager'
+            },
+            {
+                key: 'Program manager',
+                label: 'Program manager'
+            },
+            {
+                key: 'Portfolio manage',
+                label: 'Portfolio manage'
+            }
+        ]
     }
 ]
